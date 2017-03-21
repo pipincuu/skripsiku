173090c21d4a484c737d1b915b700c1be819db3f
Aisy Rozsidhy <aisymuhammad@gmail.com>
2017-03-21 16:52:25 +0700

    fix halaman

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -10,19 +10,32 @@ class Admin extends CI_Controller{
   }
 
   function index(){
-    
+
   }
 
-  function login(){
+  public function login(){
     $log = $this->Model_admin->login();
+
     if($log){
+      $array = array(
+        'id_admin' => $log->id_admin,
+        'username' => $log->username,
+        'nama'     => $log->nama
+      );
+      // print_r($array);
+      $this->session->set_userdata($array);
       redirect('HalamanUtama');
-    }else{
+    }
+    else{
       $this->session->set_flashdata('Username atau Password Salah');
       redirect('Welcome/index');
-
     }
 
+  }
+
+  public function logout(){
+    $this->session->sess_destroy();
+    redirect('Welcome', 'refresh');
   }
 
 }
