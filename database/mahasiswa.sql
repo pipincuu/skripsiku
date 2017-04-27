98d158517ff1ca80b31b5ab81da0c9624e595d2a
pipincuu <anisahmahdiya@gmail.com>
2017-04-27 09:28:16 +0700

    database dan data mahasiswa

---
+++ database/mahasiswa.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.6
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 27, 2017 at 04:23 AM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `kemahasiswaan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE IF NOT EXISTS `mahasiswa` (
+  `nim` int(11) NOT NULL,
+  `nama_mahasiswa` varchar(30) NOT NULL,
+  `program_studi` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama_mahasiswa`, `program_studi`) VALUES
+(1341180105, 'Sharfina Mahdiya Anisah', 'Teknik Informatika'),
+(1431140021, 'Nisrina Dwita Aulia', 'Manajemen Informatik'),
+(1431140009, 'Riski Anugrah Putra', 'Manajemen Informatik'),
+(1431140034, 'Ilham Akbar', 'Manajemen Informatik'),
+(1341180036, 'Miftahul Agtamas Fidyawan', 'Teknik Informatika'),
+(1341180043, 'Rachmad Jibril Al Kautsar', 'Teknik Informatika'),
+(1431140140, 'Citra Adi Negara', 'Manajemen Informatik'),
+(1431140085, 'Mahrus Ali', 'Manajemen Informatik'),
+(1341180114, 'Achmad Asa Niswara P.', 'Teknik Informatika'),
+(1341180072, 'Aditya Dwi Wardhana', 'Teknik Informatika');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
